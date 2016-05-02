064599fac671cc1f76dd8682aeb7eac69721f1c6
Cream2015 <343224963@qq.com>
2016-05-02 20:40:14 +0800

    添加低保线

---
+++ YIAN/src/YIAN/Models/LowLine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YIAN.Models
+{
+    public class LowLine
+    {
+        public int Id { get; set; }
+        public int Line { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
