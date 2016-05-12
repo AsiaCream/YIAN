2016eef9bfb1af418ba18b946f243ccca7e3b9c6
Cream2015 <343224963@qq.com>
2016-05-12 15:15:10 +0800

    Add  viewmodel about pie

---
+++ YIAN/src/YIAN/ViewModels/SituationPie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YIAN.ViewModels
+{
+    public class SituationPie
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Time { get; set; }
+        public string Town { get; set; }
+        public int Count { get; set; }
+    }
+}
